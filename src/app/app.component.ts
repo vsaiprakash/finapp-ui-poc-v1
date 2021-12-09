59ceddaff1283b0d7d049efdbe8d37435d310d4a
V. Sai Prakash <vukkumsp@gmail.com>
2021-12-09 17:50:55 +0000

    *****  LOADING SPINNER added based on ROUTER EVENTS at AppComponent level *****;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'finapp-ui-poc-v1';
+  loading: boolean;
+
+  constructor(private router: Router){
+    this.loading = false;
+    router.events.subscribe((routerEvent) => {
+      this.checkRouterEvent(routerEvent);
+    });
+  }
+
+  checkRouterEvent(routerEvent: any){
+    if(routerEvent instanceof NavigationStart) {
+      this.loading = true;
+    }
+    if(routerEvent instanceof NavigationEnd ||
+      routerEvent instanceof NavigationCancel ||
+      routerEvent instanceof NavigationError) {
+        this.loading = false;
+    }
+  }
 }
