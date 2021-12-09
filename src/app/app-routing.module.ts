3a7bfd3709b7708fa9a31f21dbcbb11af588ddbf
V. Sai Prakash <vukkumsp@gmail.com>
2021-12-09 17:02:36 +0000

    **** USED NESTED router-outlets ***** ; 404 handled with a page not found component;

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShellComponent } from './layout/shell/shell.component';
 import { AccountComponent } from './views/account/account.component';
 import { AccountsComponent } from './views/accounts/accounts.component';
 import { DaybookComponent } from './views/daybook/daybook.component';
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: 'daybook', component: DaybookComponent },
-  { path: 'accounts', component: AccountsComponent },
-  { path: 'account/:accountId', component: AccountComponent },
-  { path: '',   redirectTo: '/accounts', pathMatch: 'full' }, // redirect to `accounts`
-  // { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+  {
+    path: '', component: ShellComponent,
+    children: [
+      { path: '',   redirectTo: '/accounts', pathMatch: 'full' },
+      { path: 'daybook', component: DaybookComponent },
+      { path: 'accounts', component: AccountsComponent },
+      { path: 'account/:accountId', component: AccountComponent }
+    ]
+  },
+  { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { DaybookComponent } from './views/daybook/daybook.component';
 import { AccountsComponent } from './views/accounts/accounts.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { AccountComponent } from './views/account/account.component';
+import { ShellComponent } from './layout/shell/shell.component';
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { AccountComponent } from './views/account/account.component';
     DaybookComponent,
     AccountsComponent,
     HeaderComponent,
-    AccountComponent
+    AccountComponent,
+    ShellComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
