1435a814429665a41225c7786049ff9e250dd7dc
V. Sai Prakash <vukkumsp@gmail.com>
2021-12-10 09:28:07 +0000

    router event debugging mode disabled;

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,7 +24,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
+  imports: [RouterModule.forRoot(routes
+    // , { enableTracing: true }
+  )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
