7df8c1bfe325ee79fba9387c02825486fab1bc53
V. Sai Prakash <vukkumsp@gmail.com>
2021-12-24 08:23:43 +0000

    basic login feature added;

--- src/app/layout/login/login.component.ts
+++ src/app/layout/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { Auth } from 'aws-amplify';
 
 @Component({
   selector: 'app-login',
@@ -8,24 +11,37 @@ import { FormBuilder } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  loginForm = this.fb.group({
+  loginForm: FormGroup = this.fb.group({
     username: [''],
     password: ['']
   });
 
-  email: any;
-  username: any;
-  password: any;
 
-  constructor(private fb: FormBuilder) {
-    this.username = "";
-    this.password = "";
+  constructor(private fb: FormBuilder, private router: Router) {
   }
 
   ngOnInit(): void {
   }
 
-  loginWithCognito() {
-
-  }
+  async loginWithCognito() {
+    const username = this.loginForm.get('username')?.value;
+    const password = this.loginForm.get('password')?.value;
+    console.log(username, password);
+        try {
+          var user = await Auth.signIn(username, password);
+          console.log('Authentication performed for user=' + username + 'password=' + password+ ' login result==' + user);
+          var tokens = user.signInUserSession;
+          if (tokens != null) {
+            console.log('User authenticated');
+            this.router.navigate(['home']);
+            alert('You are logged in successfully !');
+          }
+          else {
+            console.log("tokens are null !!!");
+          }
+        } catch (error) {
+          console.log(error);
+          alert('User Authentication failed');
+        }
+      }
 }
